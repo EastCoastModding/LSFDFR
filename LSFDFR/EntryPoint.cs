0e5ea7da211b75ad102d43f885cfc9bf87c8fcae
EastCoastModding <55719564+EastCoastModding@users.noreply.github.com>
2020-01-06 01:58:54 -0500

    Add files via upload

---
+++ LSFDFR/EntryPoint.cs
@@ -0,0 +1,25 @@
+﻿[assembly: Rage.Attributes.Plugin("Los Santos Fire Department First Response", Description = "This is my first plugin.", Author = "Squad 65")]
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rage;
+using System.String;
+
+namespace MyFirstPlugin
+{
+    public static class EntryPoint
+    {
+        public static void Main()
+        {
+            Rage.GameFiber.StartNew(delegate)
+        {
+
+                Rage.Game.DisplayHelp("Welcome to LSFD:FR")
+            Rage.Game.DisplayNotification("Join the Discord Today")
+        });
+                    }
+    }
+}
